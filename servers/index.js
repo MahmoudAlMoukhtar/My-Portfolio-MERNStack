553f899e63eb8f5a6893ab4c7ce86131913f4e94
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-01-06 14:13:29 +0200

    tmp static

--- servers/index.js
+++ servers/index.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 //app.use("/api/images",express.static('public'));
-app.use('/api/images', express.static(path.join(__dirname, './tmp/public')))
+app.use('/api/images', express.static(path.join(__dirname, './tmp')))
 //app.use(cookieParser());
 //3red party middle
 helmet({
