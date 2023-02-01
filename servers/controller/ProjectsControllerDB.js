4393d7098c4fe8772eabbf7e56cfc1210a30928f
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-02-01 22:07:50 +0200

    remove particles, change image, change border main image

--- servers/controller/ProjectsControllerDB.js
+++ servers/controller/ProjectsControllerDB.js
@@ -1,91 +1,78 @@
-const Project = require('../models/ProjectModelDB');
-
-
-
-
+const Project = require("../models/ProjectModelDB");
 
 //add new student to database
-const addNewProject = (req,res,nxt)=>{
-    const {title, description, demoLink, githubLink,image} = req.body
-    const {file} = req
-   console.log(file)
-    const newProject = new Project({
-        title:title,
-        description:description,
-        demoLink:demoLink,
-        githubLink:githubLink,
-        image:image,
+const addNewProject = (req, res, nxt) => {
+  const {title, description, demoLink, githubLink, image} = req.body;
+  const {file} = req;
+  console.log(file);
+  const newProject = new Project({
+    title: title,
+    description: description,
+    demoLink: demoLink,
+    githubLink: githubLink,
+    image: req.file.path,
+  });
+  newProject
+    .save()
+    .then(() => {
+      res.status(200).send("add");
     })
-     newProject.save().then(()=>{
-        res.status(200).send("add")  
-     }).catch((err)=>{
-        res.status(400).send("error in post")  
+    .catch(err => {
+      res.status(400).send("error in post");
     });
-}
-
-
-
-
-
-
-
+};
 
 //get all student from database
-const getAllProjects = async (req, res,nxt)=>{
-    try{
-//const allStudents =  await Student.find({}).select({name:1,email:1,id:1})
-    const AllProjects = await Project.find({})
-    if(!AllProjects){
-        res.status(400).send("error in get all students")
-    }else{
-        res.json(AllProjects)
+const getAllProjects = async (req, res, nxt) => {
+  try {
+    //const allStudents =  await Student.find({}).select({name:1,email:1,id:1})
+    const AllProjects = await Project.find({});
+    if (!AllProjects) {
+      res.status(400).send("error in get all students");
+    } else {
+      res.json(AllProjects);
     }
-    }catch(err){
-        nxt(err);
-    }
-    
-}
-
+  } catch (err) {
+    nxt(err);
+  }
+};
 
 //update student by id
-const updateProjectById = async (req,res,nxt)=>{
-    const {id} = req.params
-    const updates = req.body
-    try{
-        const updateProject = await Project.findByIdAndUpdate(id, updates,{returnOriginal:false})
-    if(!updateProject){
-        res.status(400).send("error in update student")
-    }else{
-        res.status(200).send(updateProject)
+const updateProjectById = async (req, res, nxt) => {
+  const {id} = req.params;
+  const updates = req.body;
+  try {
+    const updateProject = await Project.findByIdAndUpdate(id, updates, {
+      returnOriginal: false,
+    });
+    if (!updateProject) {
+      res.status(400).send("error in update student");
+    } else {
+      res.status(200).send(updateProject);
     }
-    }catch(err){
-        nxt(err)
+  } catch (err) {
+    nxt(err);
+  }
+};
+
+const deleteProjectById = async (req, res, nxt) => {
+  const {id} = req.params;
+
+  try {
+    const studentByID = await Project.findByIdAndRemove(id);
+    if (!studentByID) {
+      res.status(400).send("errror in deleting project by id");
+    } else {
+      res.status(200).send("deleting project in successfuly");
     }
-    
-}
-
-const deleteProjectById = async (req,res,nxt)=>{
-    const {id} = req.params;
-
-    try{
-        const studentByID = await Project.findByIdAndRemove(id)
-        if(!studentByID){
-            res.status(400).send("errror in deleting project by id")
-        }else{
-            res.status(200).send("deleting project in successfuly")
-        }
-    }catch(err){
-        nxt(err)
-    }    
-}
-
-
-
-
+  } catch (err) {
+    nxt(err);
+  }
+};
 
 //get student from database by id
 // const getStudentById = async (req,res)=>{
-//     const {id} = req.params    
+//     const {id} = req.params
 //     try{
 
 //         const studentById = await Student.findById(id);
@@ -101,15 +88,9 @@ const deleteProjectById = async (req,res,nxt)=>{
 //     }
 // }
 
-
-
-
-
-
-
 module.exports = {
-    addNewProject,
-    getAllProjects,
-    updateProjectById,
-    deleteProjectById
-}
+  addNewProject,
+  getAllProjects,
+  updateProjectById,
+  deleteProjectById,
+};
